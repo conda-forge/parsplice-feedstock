0f8eafb7597b7f268e42292f60c8e147774cd440
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-01-27 08:18:03 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,4 @@ export CFLAGS="${CFLAGS} -llammps -ldb_cxx"
 cmake -DCMAKE_PREFIX_PATH="${PREFIX}"  -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../ 
 make -j${CPU_COUNT}
 make install
+cp mkhosts-slurm-nospawn.py ${PREFIX}/bin/
